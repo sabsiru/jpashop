f7bf9ac6b38cdf19ab10af1bf14dc0d16eec04c5
sabsiru <zero013@naver.com>
2024-08-31 13:41:58 +0900

    테스트 코드에서의 @Rollback 어노테이션
    롤백 어노테이션은 기본적으로 true false로 설정할시 테스트 데이터를 롤백하지 않고 DB에 그대로 둔다.

--- src/test/java/jpabook/jpashop/MemberRepositoryTest.java
+++ src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -4,6 +4,7 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
@@ -13,6 +14,7 @@ public class MemberRepositoryTest {
 
   @Test
   @Transactional
+  @Rollback(false) //@Rollback 어노테이션을 사용하면 값을 롤백하지 않는다.
   public void testMember() throws Exception{
       //given
       Member member = new Member();
@@ -26,6 +28,10 @@ public class MemberRepositoryTest {
 
       Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
       Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+      
+      //같은 영속성에서 가져오므로 TRUE 반환
+      Assertions.assertThat(findMember).isEqualTo(member);
+      System.out.println("findMember == member : " + (findMember == member));
   }
 
 }
