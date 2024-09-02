9a6979b8dd82998a89b342e467b502f93504f96e
sabsiru <zero013@naver.com>
2024-09-02 22:34:11 +0900

    jpashop 시작
    엔티티를 정의하고 해당 클래들을 생성 (category는 다음 번 강의에 추가한다.)

---
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -0,0 +1,26 @@
+package jpabook.jpashop.domain;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter @Setter
+public class Member {
+
+
+    @Id @GeneratedValue
+    private Long id;
+
+    private String name;
+
+    @Embedded // 둘 중에 하나만 있어도 됨
+    private Address address;
+
+    @OneToMany(mappedBy = "member") //읽기전용
+    private List<Order> orders = new ArrayList<>();
+}
