34cc26db7503be45236831d3c909c99ff61f7037
sabsiru <zero013@naver.com>
2024-09-03 22:26:49 +0900

    값타입 엔티티 주의점
    모든 엔티티에는 가급적 Setter를 사용하지 않는 것이 좋다.

--- src/main/java/jpabook/jpashop/domain/Address.java
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -3,13 +3,21 @@ package jpabook.jpashop.domain;
 
 import jakarta.persistence.Embeddable;
 import lombok.Getter;
-import lombok.Setter;
 
 @Embeddable
-@Getter @Setter
+@Getter //값타입은 변경 불가능하게 설계해야 한다. @Setter 제거
 public class Address {
 
     private String city;
     private String street;
     private String zipcode;
+
+    protected Address() {
+    }
+
+    public Address(String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
 }
