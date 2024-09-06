1544094fc4c9bdd2f54c4b3bf3b36141578a3a05
sabsiru <zero013@naver.com>
2024-09-06 20:19:32 +0900

    즉시로딩을 지양 하는 이유

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -17,9 +17,13 @@ public class Order {
     @Column(name = "order_id")
     private  Long id;
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "member_id")
     private Member member;
+
+    //즉시로딩을 경우 n+1조회 하기때문에 성능 저하를 유발 그러므로 가급적 LAZY, 지연로딩을 사용한다.
+    //JPQL select o from order o; -> SQL select * from order 100+1(order)
+
 
     @OneToMany(mappedBy = "order")
     private List<OrderItem> orderItems = new ArrayList<>();
