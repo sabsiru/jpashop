8fb9bd34a2d5c459f633c4896cd848190b61c89b
sabsiru <zero013@naver.com>
2025-01-06 22:30:57 +0900

    Item 관련 코드 작성

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.domain.item;
 
 import jakarta.persistence.*;
+import jpabook.jpashop.exeption.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -23,4 +24,25 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+/* 비즈니스 로직*/
+    /*
+    *  stock 증가
+    * */
+    public void addStock(int quantity) {
+        this.stockQuantity+= quantity;
+    }
+
+    /*
+    *  stock 감소
+    * */
+
+    public void removeStock(int quantity) {
+        int resStock = this.stockQuantity - quantity;
+        if (resStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = resStock;
+
+    }
 }
